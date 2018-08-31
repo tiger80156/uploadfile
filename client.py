e4e8adf678db5b54b50dba911732657f777d791c
Gaston QIU <tiger80156@gmail.com>
2018-08-31 12:03:00 +0800

    uploadFile to server

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ import requests
 files = {"file":open("lena.jpg","rb")}
 
 #Send Http request with web server
-r = requests.post("http://18.216.246.52/upload", files = files) 
-    
+r = requests.post(" http://127.0.0.1:5000/upload", files = files)
+
 print(r.status_code, r.reason)
 print(r.text[:300] + '...')
 
